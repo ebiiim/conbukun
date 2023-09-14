5f6869a118a3f16841bb83527d6b2b74ee0f0721
Shunsuke Ise <ise@ebiiim.com>
2023-09-15 01:57:31 +0900

    generatePresence

--- pkg/presence/presence.go
+++ pkg/presence/presence.go
@@ -2,6 +2,7 @@ package presence
 
 import (
 	"context"
+	"math/rand"
 	"time"
 
 	"github.com/bwmarrin/discordgo"
@@ -32,11 +33,12 @@ func PresenceUpdateLoop(ctx context.Context, s *discordgo.Session) {
 		case <-next:
 			lg.Debug().Msgf("PresenceUpdateLoop <-next")
 
-			// TODO
-			if err := SetSelfPresence(s, discordgo.StatusOnline, "お散歩", "", discordgo.ActivityTypeGame); err != nil {
+			a0, a1, a2, a3, a4 := generatePresence(time.Now())
+			if err := SetSelfPresence(s, a0, a1, a2, a3); err != nil {
 				lg.Error().Err(err).Msgf("SetSelfPresence")
 			}
-			time.AfterFunc(time.Minute*30, func() {
+			lg.Debug().Msgf("PresenceUpdateLoop wait for %s", a4)
+			time.AfterFunc(a4, func() {
 				next <- struct{}{}
 			})
 
@@ -44,8 +46,95 @@ func PresenceUpdateLoop(ctx context.Context, s *discordgo.Session) {
 	}
 }
 
+var (
+	jst = time.FixedZone("Asia/Tokyo", 9*60*60)
+)
+
+const (
+	actWalking  = "お散歩"
+	actToilet   = "トイレ"
+	actNap      = "お昼寝"
+	actFood     = "ごはん"
+	actSnack    = "おやつ"
+	actMTG      = "MTG"
+	actTraining = "運動"
+	actSleep    = "すやすや"
+)
+
+var (
+	actPrefix = map[string]bool{
+		actWalking:  true,
+		actToilet:   false,
+		actNap:      false,
+		actFood:     false,
+		actSnack:    false,
+		actMTG:      true,
+		actTraining: true,
+		actSleep:    false,
+	}
+	actDuration = map[string]time.Duration{
+		actWalking:  time.Minute * 25,
+		actToilet:   time.Minute * 5,
+		actNap:      time.Minute * 45,
+		actFood:     time.Minute * 25,
+		actSnack:    time.Minute * 15,
+		actMTG:      time.Minute * 50,
+		actTraining: time.Minute * 30,
+		actSleep:    time.Minute * 80,
+	}
+
+	actMorning = []string{actWalking, actToilet, actFood, actSnack, actTraining}
+	actEvening = []string{actWalking, actToilet, actNap, actFood, actSnack, actMTG}
+	actNight   = []string{actWalking, actToilet, actFood, actSnack, actMTG, actSleep}
+)
+
+func generatePresence(t time.Time) (status discordgo.Status, actName, actState string, actType discordgo.ActivityType, waitNext time.Duration) {
+	status = discordgo.StatusOnline
+	actState = ""
+	actType = discordgo.ActivityTypeGame
+	waitNext = time.Minute * 30
+
+	w0 := "時間帯不明" // 朝
+	w1 := "行方不明"  // お散歩
+
+	h := t.In(jst).Hour()
+	// m := t.In(jst).Minute()
+
+	switch {
+	case h >= 0 && h < 4: // 0:00-3:59
+		w0 = "深夜"
+		w1 = actNight[rand.Intn(len(actNight))]
+	case h >= 4 && h < 6: // 4:00-5:59
+		w0 = "早朝"
+		w1 = actMorning[rand.Intn(len(actMorning))]
+	case h >= 6 && h < 11: // 6:00-10:59
+		w0 = "朝"
+		w1 = actMorning[rand.Intn(len(actMorning))]
+	case h >= 11 && h < 15: // 11:00-14:59
+		w0 = "昼"
+		w1 = actEvening[rand.Intn(len(actEvening))]
+	case h >= 15 && h < 19: // 15:00-18:59
+		w0 = "夕方"
+		w1 = actEvening[rand.Intn(len(actEvening))]
+	case h >= 19 && h < 24: // 19:00-23:59
+		w0 = "夜"
+		w1 = actNight[rand.Intn(len(actNight))]
+	}
+
+	if dur, ok := actDuration[w1]; ok {
+		waitNext = dur
+	}
+	if hasPrefix, ok := actPrefix[w1]; ok && hasPrefix {
+		actName = w0 + "の" + w1 // 朝のお散歩
+	} else {
+		actName = w1 // お昼寝
+	}
+
+	return
+}
+
 func SetSelfPresence(s *discordgo.Session, status discordgo.Status, actName, actState string, actType discordgo.ActivityType) error {
-	lg.Debug().Msgf("set self presence status=%v actName=%v actState=%v", status, actName, actState)
+	lg.Info().Msgf("set self presence status=%v actName=%v actState=%v", status, actName, actState)
 
 	// FIXME: status doesn't change
 	// See also: s.UpdateGameStatus
